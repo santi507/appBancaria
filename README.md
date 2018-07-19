293804a817217bcbce5e3fb7e6d37d14f811a690
Santiago Rios <santiago.rios1758@gmail.com>
2018-07-19 00:20:46 -0500

    Inicia el reto

---
+++ README.md
@@ -0,0 +1,1 @@
+"# Reto de programaci¢n - Consultec" 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
+import { TabHiddenDirective } from '../directives/tab-hidden/tab-hidden';
+
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
@@ -17,7 +19,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    TabHiddenDirective
   ],
   imports: [
     BrowserModule,
@@ -29,7 +32,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
   ],
   providers: [
     StatusBar,
